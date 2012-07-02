6bbccbfae7de3f6a9563da23b1a32791d662aa8b
Anton Sizov <a.sysoff@gmail.com>
2012-07-03 01:42:23 +0300

    Cleanup

--- src/controller/kadmin_users_controller.erl
+++ src/controller/kadmin_users_controller.erl
@@ -28,27 +28,6 @@ logout('GET', []) ->
     boss_session:delete_session(SessionID),
     {redirect, "/login"}.
 
-%% login('GET', []) ->
-%%     ok;
-%% login('POST', []) ->
-%%     Login = Req:post_param("login"),
-%%     PasswordHash = erlang:md5(Req:post_param("password")),
-%%     case boss_db:find(kadmin_user, [{login, 'equals', Login}, {pass_hash, 'equals', PasswordHash}]) of
-%%         {error, Reason} ->
-%%             boss_flash:add(SessionID, error, "Error", io_lib:format("~p", [Reason])),
-%%             ok;
-%%         [] ->
-%%             boss_flash:add(SessionID, error, "Error", "User is not authenticated."),
-%%             {ok, [{login, Login}]};
-%%         [User] ->
-%%             boss_session:set_session_data(SessionID, principal, User:id()),
-%%             {redirect, "/"}
-%%     end.
-
-%% logout('GET', []) ->
-%%     boss_session:delete_session(SessionID),
-%%     {redirect, "/login"}.
-
 create('GET', []) ->
     ok.
 index('GET', [], {NavSpec, User}) ->
